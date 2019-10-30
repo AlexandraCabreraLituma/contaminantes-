60bbc20cfecd904f3c37129df8d661c30b157b5c
AlexandraCabreraLituma <alecabreralituma@gmail.com>
2019-10-30 12:58:18 -0500

    Add testSearchICA issue #12

--- tests/Controller/ApiObservationControllerTest.php
+++ tests/Controller/ApiObservationControllerTest.php
@@ -241,4 +241,60 @@ class ApiObservationControllerTest extends WebTestCase
     }
 
 
+    /**
+     * Implements testSearchICA200
+     * @throws \Exception
+     * @return array
+     * @covers ::searchICA
+     */
+
+    public function testSearchICA200(): array
+    {
+        $datos = [
+            'initial_time_stamp' => '2018-05-17 10:11:00-05',
+            'final_time_stamp'=>'2018-05-17 10:30:00-05',
+        ];
+        self::$client->request(
+            Request::METHOD_POST,
+            ApiObservationController::OBSERVATION_API_PATH . ApiObservationController::SEARCH. ApiObservationController::ICA,
+            [], [], [], json_encode($datos)
+        );
+        self::assertEquals(
+            Response::HTTP_OK,
+            self::$client->getResponse()->getStatusCode()
+        );
+        $cuerpo = self::$client->getResponse()->getContent();
+        $datosObservation = json_decode($cuerpo, true);
+        return $datosObservation['observations'];
+
+    }
+
+    /**
+     * Implements testSearchAdvanceObservation404
+     * @throws \Exception
+     * @return void
+     * @covers ::searchICA
+     */
+
+    public function testSearchICA404(): void
+    {
+
+        $datos = [
+            'initial_time_stamp' => '3019-05-17 10:11:00-05',
+            'final_time_stamp'=>'3019-05-17 10:30:00-05',
+        ];
+        self::$client->request(
+            Request::METHOD_POST,
+            ApiObservationController::OBSERVATION_API_PATH . ApiObservationController::SEARCH . ApiObservationController::ICA,
+            [], [], [], json_encode($datos)
+        );
+        self::assertEquals(
+            Response::HTTP_NOT_FOUND,
+            self::$client->getResponse()->getStatusCode()
+        );
+
+    }
+
+
+
 }
