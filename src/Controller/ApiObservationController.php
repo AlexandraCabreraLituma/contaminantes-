f8d76199edb71cb33c9aea48a6094716ee80ee68
AlexandraCabreraLituma <alecabreralituma@gmail.com>
2019-11-20 09:21:01 -0500

    Refactoring get stadistic variance issue #25

--- src/Controller/ApiObservationController.php
+++ src/Controller/ApiObservationController.php
@@ -315,14 +315,10 @@ class ApiObservationController extends AbstractController
                 $arr[$i] = $observations[$i]['valor'];
             }
         }
-
         $variance = 0.0;
-        // calculating mean using array_sum() method
         $average = array_sum($arr)/$num_of_elements;
         foreach($arr as $i)
         {
-            // sum of squares of differences between
-            // all numbers and means.
             $variance += pow(($i - $average), 2);
         }
         $variance=(float)($variance/$num_of_elements);
